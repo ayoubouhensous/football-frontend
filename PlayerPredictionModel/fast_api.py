627502f09e716a67eb07439e4a2172db617e8d0a
Zakaria <elguazzarzakaria10@gmail.com>
2025-05-07 19:14:51 +0100

    Fastapi

---
+++ PlayerPredictionModel/fast_api.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request
+import numpy as np
+import joblib
+
+# Load it later
+loaded_model = joblib.load('random_forest_model.pkl')
+
+app = FastAPI()
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predicts")
+async def predict(data: Request):
+    json_data = await data.json()
+    features = np.array(json_data["features"]).reshape(1, -1)
+    prediction = loaded_model.predict(features)
+    return {"prediction": prediction.tolist()[0]}
